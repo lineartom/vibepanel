f4d3a4d5bda6f03c97f4e8e633fb9147131aaf8e
tom <39309929+lineartom@users.noreply.github.com>
2026-05-21 20:09:58 -0400

    Include get-me-fabric.sh in packaging

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tar -czvf vibepanel.tar.gz -s '#^#vibepanel/#'  static templates server.py requirements.txt install.sh
+tar -czvf vibepanel.tar.gz -s '#^#vibepanel/#'  static templates server.py get-me-fabric.sh requirements.txt install.sh
 tar -tf vibepanel.tar.gz
