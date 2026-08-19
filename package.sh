a54aed700bef1ba1eb8fab4d21b57168c8eb640c
tom <39309929+lineartom@users.noreply.github.com>
2026-08-18 22:19:26 -0400

    github: release workflow and compatible package.sh

--- package.sh
+++ package.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-VERSION=$(git describe)
+set -eu
+
+VERSION=${VERSION:-$(git describe)}
 FILENAME=vibepanel-$VERSION.tar.gz
 
+FILES="static templates server.py get-me-fabric.sh requirements.txt install.sh vibepanel.service VERSION"
+
 echo "$VERSION" > VERSION
-tar -czvf "$FILENAME" -s '#^#vibepanel/#'  static templates server.py get-me-fabric.sh requirements.txt install.sh vibepanel.service VERSION
+
+# Both tars can prefix paths with vibepanel/, but spell it differently:
+# GNU (Linux, the CI runners) wants --transform, BSD (macOS) wants -s.
+if tar --version 2>/dev/null | grep -q GNU; then
+    tar -czf "$FILENAME" --transform 's#^#vibepanel/#' $FILES
+else
+    tar -czf "$FILENAME" -s '#^#vibepanel/#' $FILES
+fi
+
 rm VERSION
 tar -tf "$FILENAME"
